b7a977ef2e2a687bc3d9d1bf752e94b66dc5ddc6
sacrilegioushero220 <sandweepsabu@gmail.com>
2023-04-04 15:39:33 +0530

    Both side and bottom panels are fixed with rounded corner boxes

--- lib/Pages/myhomepage.dart
+++ lib/Pages/myhomepage.dart
@@ -8,70 +8,89 @@ class MyHomePage extends StatelessWidget {
     return SafeArea(
       child: Scaffold(
           body: Container(
-        color: const Color.fromARGB(255, 255, 255, 255),
+        color: const Color(0xFF040404),
         child: Center(
           child: Column(
             children: [
               Container(
-                color: Colors.blue,
+                color: const Color(0xFF040404),
                 height: 720,
                 width: 1280,
                 child: Row(
                   children: [
-                    Container(
-                      color: const Color.fromARGB(255, 2, 71, 43),
+                    SizedBox(
+                      //color: const Color(0xFF040404),
                       height: 810,
                       width: 200,
+
                       child: Container(
-                        child: Column(
-                          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                          children: [
-                            Expanded(
-                              child: Image.asset(
-                                'assets/icons/page1/0935.png',
-                                height: 65,
-                                width: 65,
+                        child: Padding(
+                          padding: const EdgeInsets.all(10),
+                          child: Padding(
+                            padding: const EdgeInsets.all(8.0),
+                            child: Container(
+                              decoration: BoxDecoration(
+                                color: const Color(0xFF040404),
+                                borderRadius: BorderRadius.circular(15),
+                                border: Border.all(
+                                  color: Colors.grey,
+                                  width: 2,
+                                ),
+                              ),
+                              child: Column(
+                                mainAxisAlignment:
+                                    MainAxisAlignment.spaceEvenly,
+                                children: [
+                                  const SizedBox(height: 10),
+                                  Expanded(
+                                    child: Image.asset(
+                                      'assets/icons/page1/0935.png',
+                                      height: 65,
+                                      width: 65,
+                                    ),
+                                  ),
+                                  const SizedBox(height: 10),
+                                  Expanded(
+                                    child: Image.asset(
+                                      'assets/icons/page1/Path 1.png',
+                                      height: 65,
+                                      width: 65,
+                                    ),
+                                  ),
+                                  const SizedBox(height: 10),
+                                  Expanded(
+                                    child: Image.asset(
+                                      'assets/icons/page1/play-button.png',
+                                      height: 65,
+                                      width: 65,
+                                    ),
+                                  ),
+                                  const SizedBox(height: 10),
+                                  Expanded(
+                                    child: Image.asset(
+                                      'assets/icons/page1/car.png',
+                                      height: 65,
+                                      width: 65,
+                                    ),
+                                  ),
+                                  const SizedBox(height: 10),
+                                  Expanded(
+                                    child: Image.asset(
+                                      'assets/icons/page1/app.png',
+                                      height: 65,
+                                      width: 65,
+                                    ),
+                                  ),
+                                  const SizedBox(height: 10),
+                                ],
                               ),
                             ),
-                            const SizedBox(height: 10),
-                            Expanded(
-                              child: Image.asset(
-                                'assets/icons/page1/Path 1.png',
-                                height: 65,
-                                width: 65,
-                              ),
-                            ),
-                            const SizedBox(height: 10),
-                            Expanded(
-                              child: Image.asset(
-                                'assets/icons/page1/play-button.png',
-                                height: 65,
-                                width: 65,
-                              ),
-                            ),
-                            const SizedBox(height: 10),
-                            Expanded(
-                              child: Image.asset(
-                                'assets/icons/page1/car.png',
-                                height: 65,
-                                width: 65,
-                              ),
-                            ),
-                            const SizedBox(height: 10),
-                            Expanded(
-                              child: Image.asset(
-                                'assets/icons/page1/app.png',
-                                height: 65,
-                                width: 65,
-                              ),
-                            ),
-                            const SizedBox(height: 10),
-                          ],
+                          ),
                         ),
                       ),
                     ),
                     Container(
-                      color: const Color.fromARGB(255, 196, 184, 17),
+                      color: const Color(0xFF040404),
                       height: 806,
                       width: 1080,
                       //child: Image.asset('assets/icons/page1/app.png'),
@@ -80,68 +99,85 @@ class MyHomePage extends StatelessWidget {
                 ),
               ),
               Container(
-                color: const Color.fromARGB(255, 33, 243, 156),
+                color: const Color(0xFF040404),
                 height: 100,
                 width: 1280,
-                child: Row(
-                  children: [
-                    Expanded(
-                      child: Image.asset(
-                        'assets/icons/page1/steering-wheel.png',
-                        height: 50,
-                        width: 50,
+                child: Padding(
+                  padding: const EdgeInsets.fromLTRB(10, 0, 10, 0),
+                  child: Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Container(
+                      decoration: BoxDecoration(
+                        color: const Color(0xFF040404),
+                        borderRadius: BorderRadius.circular(15),
+                        border: Border.all(
+                          color: Colors.grey,
+                          width: 2,
+                        ),
+                      ),
+                      child: Row(
+                        children: [
+                          const SizedBox(height: 10),
+                          Expanded(
+                            child: Image.asset(
+                              'assets/icons/page1/steering-wheel.png',
+                              height: 50,
+                              width: 50,
+                            ),
+                          ),
+                          const SizedBox(height: 10),
+                          Expanded(
+                            child: Image.asset(
+                              'assets/icons/page1/heat.png',
+                              height: 50,
+                              width: 50,
+                            ),
+                          ),
+                          const SizedBox(height: 10),
+                          Expanded(
+                            child: Image.asset(
+                              'assets/icons/page1/Group 1.png',
+                              height: 55,
+                              width: 55,
+                            ),
+                          ),
+                          const SizedBox(height: 10),
+                          Expanded(
+                            child: Image.asset(
+                              'assets/icons/page1/fan.png',
+                              height: 50,
+                              width: 50,
+                            ),
+                          ),
+                          const SizedBox(height: 10),
+                          Expanded(
+                            child: Image.asset(
+                              'assets/icons/page1/Group 2.png',
+                              height: 55,
+                              width: 55,
+                            ),
+                          ),
+                          const SizedBox(height: 10),
+                          Expanded(
+                            child: Image.asset(
+                              'assets/icons/page1/APS.png',
+                              height: 50,
+                              width: 50,
+                            ),
+                          ),
+                          const SizedBox(height: 10),
+                          Expanded(
+                            child: Image.asset(
+                              'assets/icons/page1/sport-car.png',
+                              height: 40,
+                              width: 40,
+                            ),
+                          ),
+                          const SizedBox(height: 10),
+                        ],
                       ),
                     ),
-                    const SizedBox(height: 10),
-                    Expanded(
-                      child: Image.asset(
-                        'assets/icons/page1/heat.png',
-                        height: 50,
-                        width: 50,
-                      ),
-                    ),
-                    const SizedBox(height: 10),
-                    Expanded(
-                      child: Image.asset(
-                        'assets/icons/page1/Group 1.png',
-                        height: 55,
-                        width: 55,
-                      ),
-                    ),
-                    const SizedBox(height: 10),
-                    Expanded(
-                      child: Image.asset(
-                        'assets/icons/page1/fan.png',
-                        height: 50,
-                        width: 50,
-                      ),
-                    ),
-                    const SizedBox(height: 10),
-                    Expanded(
-                      child: Image.asset(
-                        'assets/icons/page1/Group 2.png',
-                        height: 55,
-                        width: 55,
-                      ),
-                    ),
-                    const SizedBox(height: 10),
-                    Expanded(
-                      child: Image.asset(
-                        'assets/icons/page1/APS.png',
-                        height: 50,
-                        width: 50,
-                      ),
-                    ),
-                    const SizedBox(height: 10),
-                    Expanded(
-                      child: Image.asset(
-                        'assets/icons/page1/sport-car.png',
-                        height: 40,
-                        width: 40,
-                      ),
-                    ),
-                    const SizedBox(height: 10),
-                  ],
+                  ),
                 ),
               )
             ],
