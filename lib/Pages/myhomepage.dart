12b90332f057cd41a71a7f0df3cb4502a49dc70d
sacrilegioushero220 <sandweepsabu@gmail.com>
2023-04-04 14:46:03 +0530

    Icons are added to the first page

--- lib/Pages/myhomepage.dart
+++ lib/Pages/myhomepage.dart
@@ -22,11 +22,59 @@ class MyHomePage extends StatelessWidget {
                       color: const Color.fromARGB(255, 2, 71, 43),
                       height: 810,
                       width: 200,
+                      child: Container(
+                        child: Column(
+                          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                          children: [
+                            Expanded(
+                              child: Image.asset(
+                                'assets/icons/page1/0935.png',
+                                height: 65,
+                                width: 65,
+                              ),
+                            ),
+                            const SizedBox(height: 10),
+                            Expanded(
+                              child: Image.asset(
+                                'assets/icons/page1/Path 1.png',
+                                height: 65,
+                                width: 65,
+                              ),
+                            ),
+                            const SizedBox(height: 10),
+                            Expanded(
+                              child: Image.asset(
+                                'assets/icons/page1/play-button.png',
+                                height: 65,
+                                width: 65,
+                              ),
+                            ),
+                            const SizedBox(height: 10),
+                            Expanded(
+                              child: Image.asset(
+                                'assets/icons/page1/car.png',
+                                height: 65,
+                                width: 65,
+                              ),
+                            ),
+                            const SizedBox(height: 10),
+                            Expanded(
+                              child: Image.asset(
+                                'assets/icons/page1/app.png',
+                                height: 65,
+                                width: 65,
+                              ),
+                            ),
+                            const SizedBox(height: 10),
+                          ],
+                        ),
+                      ),
                     ),
                     Container(
                       color: const Color.fromARGB(255, 196, 184, 17),
                       height: 806,
                       width: 1080,
+                      //child: Image.asset('assets/icons/page1/app.png'),
                     ),
                   ],
                 ),
@@ -35,7 +83,66 @@ class MyHomePage extends StatelessWidget {
                 color: const Color.fromARGB(255, 33, 243, 156),
                 height: 100,
                 width: 1280,
-                child: Row(),
+                child: Row(
+                  children: [
+                    Expanded(
+                      child: Image.asset(
+                        'assets/icons/page1/steering-wheel.png',
+                        height: 50,
+                        width: 50,
+                      ),
+                    ),
+                    const SizedBox(height: 10),
+                    Expanded(
+                      child: Image.asset(
+                        'assets/icons/page1/heat.png',
+                        height: 50,
+                        width: 50,
+                      ),
+                    ),
+                    const SizedBox(height: 10),
+                    Expanded(
+                      child: Image.asset(
+                        'assets/icons/page1/Group 1.png',
+                        height: 55,
+                        width: 55,
+                      ),
+                    ),
+                    const SizedBox(height: 10),
+                    Expanded(
+                      child: Image.asset(
+                        'assets/icons/page1/fan.png',
+                        height: 50,
+                        width: 50,
+                      ),
+                    ),
+                    const SizedBox(height: 10),
+                    Expanded(
+                      child: Image.asset(
+                        'assets/icons/page1/Group 2.png',
+                        height: 55,
+                        width: 55,
+                      ),
+                    ),
+                    const SizedBox(height: 10),
+                    Expanded(
+                      child: Image.asset(
+                        'assets/icons/page1/APS.png',
+                        height: 50,
+                        width: 50,
+                      ),
+                    ),
+                    const SizedBox(height: 10),
+                    Expanded(
+                      child: Image.asset(
+                        'assets/icons/page1/sport-car.png',
+                        height: 40,
+                        width: 40,
+                      ),
+                    ),
+                    const SizedBox(height: 10),
+                  ],
+                ),
               )
             ],
           ),
