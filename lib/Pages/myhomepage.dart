abfde8f4a72c01f7b7fbf4bc770e7c94ca2496f8
sacrilegioushero220 <sandweepsabu@gmail.com>
2023-04-04 13:32:17 +0530

    layout is enlarged for more covenience

--- lib/Pages/myhomepage.dart
+++ lib/Pages/myhomepage.dart
@@ -14,26 +14,26 @@ class MyHomePage extends StatelessWidget {
             children: [
               Container(
                 color: Colors.blue,
-                height: 740,
+                height: 720,
                 width: 1280,
                 child: Row(
                   children: [
                     Container(
                       color: const Color.fromARGB(255, 2, 71, 43),
                       height: 810,
-                      width: 90,
+                      width: 200,
                     ),
                     Container(
                       color: const Color.fromARGB(255, 196, 184, 17),
                       height: 806,
-                      width: 1190,
+                      width: 1080,
                     ),
                   ],
                 ),
               ),
               Container(
                 color: const Color.fromARGB(255, 33, 243, 156),
-                height: 80,
+                height: 100,
                 width: 1280,
                 child: Row(),
               )
