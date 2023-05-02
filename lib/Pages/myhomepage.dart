7d576ba90e705bd486eff7d84d82dcdf8f93d7af
sacrilegioushero220 <sandweepsabu@gmail.com>
2023-05-02 06:32:55 +0530

    UI Updated with inital latest ui design

--- lib/Pages/myhomepage.dart
+++ lib/Pages/myhomepage.dart
@@ -12,192 +12,194 @@ class MyHomePage extends StatelessWidget {
           body: Container(
         color: const Color(0xFF040404),
         child: Center(
-          child: Column(
-            children: [
-              Container(
-                color: const Color(0xFF040404),
-                height: 720,
-                width: 1280,
-                child: Row(
-                  children: [
-                    SizedBox(
-                      //color: const Color(0xFF040404),
-                      height: 810,
-                      width: 200,
+          child: Expanded(
+            child: Column(
+              children: [
+                Expanded(
+                  child: Container(
+                    color: const Color(0xFF040404),
+                    height: 720,
+                    width: 1280,
+                    child: Row(
+                      children: [
+                        SizedBox(
+                          //color: const Color(0xFF040404),
+                          height: 810,
+                          width: 200,
 
-                      child: Container(
-                        child: Padding(
-                          padding: const EdgeInsets.all(10),
                           child: Padding(
-                            padding: const EdgeInsets.all(8.0),
-                            child: Container(
-                              decoration: BoxDecoration(
-                                color: const Color(0xFF040404),
-                                borderRadius: BorderRadius.circular(15),
-                                border: Border.all(
-                                  color: Colors.grey,
-                                  width: 2,
+                            padding: const EdgeInsets.all(10),
+                            child: Padding(
+                              padding: const EdgeInsets.all(8.0),
+                              child: Container(
+                                decoration: BoxDecoration(
+                                  color: const Color(0xFF040404),
+                                  borderRadius: BorderRadius.circular(15),
+                                  border: Border.all(
+                                    color: Colors.grey,
+                                    width: 2,
+                                  ),
+                                ),
+                                child: Column(
+                                  mainAxisAlignment:
+                                      MainAxisAlignment.spaceEvenly,
+                                  children: [
+                                    const SizedBox(height: 10),
+                                    Expanded(
+                                      child: Image.asset(
+                                        'assets/icons/page1/0935.png',
+                                        height: 65,
+                                        width: 65,
+                                      ),
+                                    ),
+                                    const SizedBox(height: 10),
+                                    Expanded(
+                                      child: Image.asset(
+                                        'assets/icons/page1/Path 1.png',
+                                        height: 65,
+                                        width: 65,
+                                      ),
+                                    ),
+                                    const SizedBox(height: 10),
+                                    Expanded(
+                                      child: Image.asset(
+                                        'assets/icons/page1/play-button.png',
+                                        height: 65,
+                                        width: 65,
+                                      ),
+                                    ),
+                                    const SizedBox(height: 10),
+                                    Expanded(
+                                      child: Image.asset(
+                                        'assets/icons/page1/car.png',
+                                        height: 65,
+                                        width: 65,
+                                      ),
+                                    ),
+                                    const SizedBox(height: 10),
+                                    Expanded(
+                                      child: Image.asset(
+                                        'assets/icons/page1/app.png',
+                                        height: 65,
+                                        width: 65,
+                                      ),
+                                    ),
+                                    const SizedBox(height: 10),
+                                  ],
                                 ),
                               ),
-                              child: Column(
-                                mainAxisAlignment:
-                                    MainAxisAlignment.spaceEvenly,
-                                children: [
-                                  const SizedBox(height: 10),
-                                  Expanded(
-                                    child: Image.asset(
-                                      'assets/icons/page1/0935.png',
-                                      height: 65,
-                                      width: 65,
-                                    ),
-                                  ),
-                                  const SizedBox(height: 10),
-                                  Expanded(
-                                    child: Image.asset(
-                                      'assets/icons/page1/Path 1.png',
-                                      height: 65,
-                                      width: 65,
-                                    ),
-                                  ),
-                                  const SizedBox(height: 10),
-                                  Expanded(
-                                    child: Image.asset(
-                                      'assets/icons/page1/play-button.png',
-                                      height: 65,
-                                      width: 65,
-                                    ),
-                                  ),
-                                  const SizedBox(height: 10),
-                                  Expanded(
-                                    child: Image.asset(
-                                      'assets/icons/page1/car.png',
-                                      height: 65,
-                                      width: 65,
-                                    ),
-                                  ),
-                                  const SizedBox(height: 10),
-                                  Expanded(
-                                    child: Image.asset(
-                                      'assets/icons/page1/app.png',
-                                      height: 65,
-                                      width: 65,
-                                    ),
-                                  ),
-                                  const SizedBox(height: 10),
-                                ],
-                              ),
                             ),
                           ),
                         ),
-                      ),
-                    ),
-                    Container(
-                      color: const Color(0xFF040404),
-                      height: 806,
-                      width: 1080,
-                      child: Container(
-                        child: Image.asset(
-                          'assets/icons/page1/car34.png',
-                          height: 100,
-                          width: 100,
+                        Flexible(
+                          child: Container(
+                            color: const Color(0xFF040404),
+                            height: 806,
+                            width: 1080,
+                            child: Image.asset(
+                              'assets/icons/page1/car34.png',
+                              height: 100,
+                              width: 100,
+                            ),
+                          ),
                         ),
-                      ),
-                    ),
-                  ],
-                ),
-              ),
-              Container(
-                color: const Color(0xFF040404),
-                height: 100,
-                width: 1280,
-                child: Padding(
-                  padding: const EdgeInsets.fromLTRB(10, 0, 10, 0),
-                  child: Padding(
-                    padding: const EdgeInsets.all(8.0),
-                    child: Container(
-                      decoration: BoxDecoration(
-                        color: const Color(0xFF040404),
-                        borderRadius: BorderRadius.circular(15),
-                        border: Border.all(
-                          color: Colors.grey,
-                          width: 2,
-                        ),
-                      ),
-                      child: Row(
-                        children: [
-                          const SizedBox(height: 10),
-                          Expanded(
-                            child: Image.asset(
-                              'assets/icons/page1/steering-wheel.png',
-                              height: 50,
-                              width: 50,
-                            ),
-                          ),
-                          const SizedBox(height: 10),
-                          Expanded(
-                            child: Image.asset(
-                              'assets/icons/page1/heat.png',
-                              height: 50,
-                              width: 50,
-                            ),
-                          ),
-                          const SizedBox(height: 10),
-                          Expanded(
-                            child: Image.asset(
-                              'assets/icons/page1/Group 1.png',
-                              height: 55,
-                              width: 55,
-                            ),
-                          ),
-                          const SizedBox(height: 10),
-                          Expanded(
-                            child: Image.asset(
-                              'assets/icons/page1/fan.png',
-                              height: 50,
-                              width: 50,
-                            ),
-                          ),
-                          const SizedBox(height: 10),
-                          Expanded(
-                            child: Image.asset(
-                              'assets/icons/page1/Group 2.png',
-                              height: 55,
-                              width: 55,
-                            ),
-                          ),
-                          const SizedBox(height: 10),
-                          Expanded(
-                            child: IconButton(
-                              onPressed: () {
-                                Navigator.push(
-                                  context,
-                                  MaterialPageRoute(
-                                      builder: (context) => const ApsPage()),
-                                );
-                              },
-                              icon: Image.asset(
-                                'assets/icons/page1/APS.png',
-                                height: 60,
-                                width: 60,
-                              ),
-                            ),
-                          ),
-                          const SizedBox(height: 10),
-                          Expanded(
-                            child: Image.asset(
-                              'assets/icons/page1/sport-car.png',
-                              height: 40,
-                              width: 40,
-                            ),
-                          ),
-                          const SizedBox(height: 10),
-                        ],
-                      ),
+                      ],
                     ),
                   ),
                 ),
-              )
-            ],
+                Container(
+                  color: const Color(0xFF040404),
+                  height: 100,
+                  width: 1280,
+                  child: Padding(
+                    padding: const EdgeInsets.fromLTRB(10, 0, 10, 0),
+                    child: Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: Container(
+                        decoration: BoxDecoration(
+                          color: const Color(0xFF040404),
+                          borderRadius: BorderRadius.circular(15),
+                          border: Border.all(
+                            color: Colors.grey,
+                            width: 2,
+                          ),
+                        ),
+                        child: Row(
+                          children: [
+                            const SizedBox(height: 10),
+                            Expanded(
+                              child: Image.asset(
+                                'assets/icons/page1/steering-wheel.png',
+                                height: 50,
+                                width: 50,
+                              ),
+                            ),
+                            const SizedBox(height: 10),
+                            Expanded(
+                              child: Image.asset(
+                                'assets/icons/page1/heat.png',
+                                height: 50,
+                                width: 50,
+                              ),
+                            ),
+                            const SizedBox(height: 10),
+                            Expanded(
+                              child: Image.asset(
+                                'assets/icons/page1/Group 1.png',
+                                height: 55,
+                                width: 55,
+                              ),
+                            ),
+                            const SizedBox(height: 10),
+                            Expanded(
+                              child: Image.asset(
+                                'assets/icons/page1/fan.png',
+                                height: 50,
+                                width: 50,
+                              ),
+                            ),
+                            const SizedBox(height: 10),
+                            Expanded(
+                              child: Image.asset(
+                                'assets/icons/page1/Group 2.png',
+                                height: 55,
+                                width: 55,
+                              ),
+                            ),
+                            const SizedBox(height: 10),
+                            Expanded(
+                              child: IconButton(
+                                onPressed: () {
+                                  Navigator.push(
+                                    context,
+                                    MaterialPageRoute(
+                                        builder: (context) => const ApsPage()),
+                                  );
+                                },
+                                icon: Image.asset(
+                                  'assets/icons/page1/APS.png',
+                                  height: 60,
+                                  width: 60,
+                                ),
+                              ),
+                            ),
+                            const SizedBox(height: 10),
+                            Expanded(
+                              child: Image.asset(
+                                'assets/icons/page1/sport-car.png',
+                                height: 40,
+                                width: 40,
+                              ),
+                            ),
+                            const SizedBox(height: 10),
+                          ],
+                        ),
+                      ),
+                    ),
+                  ),
+                )
+              ],
+            ),
           ),
         ),
       )),
